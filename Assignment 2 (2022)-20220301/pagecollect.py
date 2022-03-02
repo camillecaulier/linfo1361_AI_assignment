b2afe2b0ed2dbb6d486c2402e11c4fa220e2a864
camillecaulier <camillecaulier.cc@gmail.com>
2022-03-02 18:47:21 +0100

    modification

--- Assignment 2 (2022)-20220301/pagecollect.py
+++ Assignment 2 (2022)-20220301/pagecollect.py
@@ -14,28 +14,43 @@ class PageCollect(Problem):
         #it is worth finding the position of p once because index() is a linear function and can bottleneck
         #position[0] = row , position[1] = column
 
+        no_paper_left = 1 #we have not achieved the final goal untill
+
         # pass
 
     def actions(self, state):
         """we can only move one square at a time north east south west"""
         possible_locations = [[0, 1], [0, -1], [1, 0], [-1, 0]] #right left down up
 
-        row = problem.position[0] #y
-        column = problem.position[1] #x
+        position = find_position(state.grid)#this very costly since it is O(nxm)
+
+        row = position[0] #y
+        column = position[1] #x
 
         action_list = []
         for [i,j] in possible_locations:
             new_row = row +i
             new_column = column + j
+            #check if the action is legal
             if(state.grid[new_row][new_column] != '#' and new_row < len(state.grid) and new_column< len(state.grid[0])):
-                action_list.append([new_row])
-        pass
+                action_list.append([new_row,new_column, position]) #we store the old position to avoid redoing a search
+
+        return action_list
     
     def result(self, state, action):
+        new_grid = state.grid
+        new_row, new_column,last_position  = action
 
+        #move the person
+        #erase the person
+        new_grid[last_position[0]][last_position[1]] = ' '
+        #move the person
+        if(new_grid[new_row][new_column] ==)
         pass
 
     def goal_test(self, state):
+        #goal if one of the paper has been removed
+        #when all paper are gone we have a goal when we are in the class room
         pass
     
     def h(self, node):
